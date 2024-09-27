600acc6c5965a80afd964598897f5b43776ad59e
Ignacio Mongi <ignaciomg3@gmail.com>
2024-09-27 00:16:53 -0300

    Tp2 - Menú de opciones terminado.

--- TP2/src/Main.java
+++ TP2/src/Main.java
@@ -1,46 +1,152 @@
+import java.io.File;
 import java.util.Scanner;
 import controladores.ProcesadorDeTexto;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class Main {
     public static void main(String[] args) {
 
+        //Paso 1 ) eliminar archivos en la carpeta archivoTexto
+        String rutaYArchivo = "./TP2/src/archivoTexto/Frases.txt";
+
+        File archivo = new File(rutaYArchivo);
+        if (archivo.delete()) {
+            System.out.println("El archivo ha sido eliminado exitosamente.");
+        } else {
+            System.out.println("No se pudo eliminar el archivo. Asegúrate de que existe y no está en uso.");
+        }
+
+        //Paso 2 crear ProcesadorDeTexto
+        ProcesadorDeTexto nuevoProcesador = new ProcesadorDeTexto();
+        nuevoProcesador.setFileName(rutaYArchivo);
+
         //Punto A
         mostrarBienvenida();
-
-        //crear un objeto de la clase ProcesadordeTexto que reciba por parámetro un string
-        ProcesadorDeTexto procesador = new ProcesadorDeTexto();
-
-        //Punto B
-        procesador.ingresarTexto();
-        String textoIngresado = procesador.texto;
-        System.out.println("Texto ingresado: " + textoIngresado);
-
-        //Punto C - Cantidad Palabras
-        int cantidadDePalabras = procesador.contarPalabras();
-        System.out.println("Cantidad de palabras: " + cantidadDePalabras);
-
-        //Punto D - cantidad de vocales
-        int cantidadDeVocales = procesador.contarVocales();
-        System.out.println("Cantidad de vocales: " + cantidadDeVocales);
-
-        //Punto E - cantidad de consonantes
-        int cantidadDeConsonantes = procesador.contarConsonantes();
-        System.out.println("Cantidad de consonantes: " + cantidadDeConsonantes);
-
-        //Punto F - Validar cantidad de caracteres
-
-        //Punto G - Permitir al usuario guardar las frases
-        //despedida
-        System.out.println("Gracias por utilizar la aplicación de procesamiento de texto");
+        menuOpciones(nuevoProcesador);
     }
 
     public static void mostrarBienvenida() {
-        System.out.println("Bienvenido a la aplicación de procesamiento de texto");
+        System.out.println("Bienvenido a la aplicación de procesamiento de texto...");
     }
-    public static String ingresarTexto() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Por favor, ingrese el texto:");
-        return scanner.nextLine();
+    public static void menuOpciones(ProcesadorDeTexto procesador) {
+
+        Scanner nuevoScanner = new Scanner(System.in);
+        boolean continuar = true;
+
+        while (continuar) {
+            System.out.println("\n ********************* Menú de Opciones: **********************");
+            System.out.println("\n El programa funciona si se siguen las siguientes opciones en orden.");
+
+            System.out.println(" a) Mensaje de Bienvenida");
+            System.out.println(" b) Ingresar Texto que termine en punto (puede contener varias oraciones separadas por punto)");
+            System.out.println(" c) Cantidad de Palabras separadas por espacio.");
+            System.out.println(" d) Cantidad de Vocales");
+            System.out.println(" e) Cantidad de Consonantes");
+            System.out.println(" f) Validar cantidad de caracteres");
+            System.out.println(" g) Guardar las Frases separadas por punto");
+            System.out.println(" h) Imprimir el total de frases separadas por punto");
+            System.out.println(" i) Seleccionar frases");
+            System.out.println(" j) Generar fichero txt/csv con las frases\n");
+            System.out.println(" Presione cualquier letra para Salir \n");
+            System.out.print(" Seleccione una opción: ");
+
+            char opcion = nuevoScanner.next().toLowerCase().charAt(0);
+
+            switch (opcion) {
+                case 'a':
+                    System.out.println("Opción: Mensaje de Bienvenida");
+                    // Llamar a la función correspondiente
+                    mostrarBienvenida();
+                    break;
+                case 'b':
+                    System.out.println("Opción: Ingresar Texto");
+                    // Llamar a la función correspondiente
+                    //Punto B
+                    procesador.ingresarTexto();
+                    //acceder a getTexto(), acceder al método.
+                    //aplicando encapsulamiento.
+                    String textoIngresado = procesador.getTexto();
+                    System.out.println("Texto ingresado: " + textoIngresado);
+                    break;
+                case 'c':
+                    System.out.println("Opción: Cantidad de Palabras");
+                    // Llamar a la función correspondiente
+                    //Punto C - Cantidad Palabras
+                    int cantidadDePalabras = procesador.contarPalabras();
+                    System.out.println("Cantidad de palabras: " + cantidadDePalabras);
+                    break;
+                case 'd':
+                    System.out.println("Opción: Cantidad de Vocales");
+                    // Llamar a la función correspondiente
+                    //Punto D - cantidad de vocales
+                    int cantidadDeVocales = procesador.contarVocales();
+                    System.out.println("Cantidad de vocales: " + cantidadDeVocales);
+                    break;
+                case 'e':
+                    System.out.println("Opción: Cantidad de Consonantes");
+                    // Llamar a la función correspondiente
+                    //Punto E - cantidad de consonantes
+                    int cantidadDeConsonantes = procesador.contarConsonantes();
+                    System.out.println("Cantidad de consonantes: " + cantidadDeConsonantes);
+
+                    break;
+                case 'f':
+                    System.out.println("Opción: Validar cantidad de caracteres");
+                    // Llamar a la función correspondiente
+                    if (procesador.validarLongitud()) {
+                        System.out.println("El texto ingresado no supera los 140 caracteres");
+                    } else {
+                        System.out.println("Error: el texto supera los 140 caracteres");
+                    }
+                    break;
+                case 'g':
+                    System.out.println("Opción: Guardar las frases, las frases terminan con .,?!;");
+                    // Llamar a la función correspondiente
+                    //Punto G - Permitir al usuario guardar las frases
+                    //procesador.guardarFrases();
+                    procesador.AgregarFrasesALaLista();
+                    procesador.EnumerarFrases();
+
+
+                    break;
+                case 'h':
+                    System.out.println("Opción: Imprimir el total de frases");
+                    // Llamar a la función correspondiente
+                    procesador.EnumerarFrases();
+
+                    break;
+                case 'i':
+                    System.out.println("Opción: Seleccionar frases");
+                    // Llamar a la función correspondiente
+                    //Punto I - Permitir al usuario seleccionar frases de un listado, editarlas y guardarlas
+                    procesador.EnumerarFrases();
+                    procesador.seleccionarFrase();
+                    procesador.EnumerarFrases();
+
+                    break;
+                case 'j':
+                    System.out.println("Opción: Generar fichero txt/csv con las frases");
+                    // Llamar a la función correspondiente
+                    //crear archivo  txt/csv con las frases
+                    procesador.generarFichcero();
+                    procesador.imprimirTotalDeFrases();
+
+                    break;
+                default:
+                    System.out.println("Saliendo del programa...\n Gracias por utilizar la aplicación de procesamiento de texto");
+                    continuar = false;
+                    break;
+            }
+        }
+        nuevoScanner.close();
     }
+
+    //función Menú de opciones.
+    //función para mostrar opciones
+
+
 
     /*"P n2
     Consigna
@@ -56,6 +162,9 @@ public class Main {
     g- Permitir al usuario guardar las frases
     h- Imprimir por pantalla el total de frases
     i- Permitir al usuario seleccionar frases de un listado, editarlas y guardarlas
-    j- Generar fichero txt/csv con las frases"*/
+    j- Generar fichero txt/csv con las frases"
+
+    mandar ahí: liferreira90@gmail.com */
+
 
 }
--- TP2/src/controladores/ProcesadorDeTexto.java
+++ TP2/src/controladores/ProcesadorDeTexto.java
@@ -1,12 +1,20 @@
 package controladores;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProcesadorDeTexto {
-    // Atributos
-    public String texto;
+    // Atributos puedo hacerlos privado.
+    private String texto;
     public int cantidadPalabras;
     public int cantidadVocales;
     public int cantidadConsonantes;
+    public List<String> listaFrases;
+    private String fileName;
 
     // Constructor
     public ProcesadorDeTexto() {
@@ -14,20 +22,27 @@ public class ProcesadorDeTexto {
         this.cantidadPalabras = 0;
         this.cantidadVocales = 0;
         this.cantidadConsonantes = 0;
-    }
-    //crear un metodo que no sea static
+        this.listaFrases = new ArrayList<>();
+        this.fileName = "";
 
+    }
+
+    Scanner scanner = new Scanner(System.in);
+    // Métodos
+    //comentarios: puedo hacerlos static.
+    //Comentario: puedo usar el mismo objeto Scanner en toda la clase.
 
     public void  ingresarTexto() {
-        Scanner scanner = new Scanner(System.in);
+        //Ccomentario: puedo usar el mismo objeto Scanner en toda la clase.
+        //Scanner scanner = new Scanner(System.in);
         System.out.println("Por favor, ingrese el texto:");
         String textoIngresado = scanner.nextLine();
-        boolean esValido = validarLongitud(textoIngresado);
+        boolean esValido = validarLongitud();
         //si validarLongitud() devuelve true entonces se ejecuta el this.texto = scanner.nextLine(); de lo contrario volver a pedir
         while (!esValido) {
             System.out.println("Error: el texto no puede superar los 140 caracteres. Por favor, ingrese nuevamente:");
             textoIngresado = scanner.nextLine();
-            esValido = validarLongitud(textoIngresado);
+            esValido = validarLongitud();
         }
         this.texto = textoIngresado;
     }
@@ -51,7 +66,7 @@ public class ProcesadorDeTexto {
 
     public int contarConsonantes() {
         int contador = 0;
-        //haceme una lista de letras consonantes
+
         String textoMinusculas = this.texto.toLowerCase();
         String listaConsonantes = "bcdfghjklmnñpqrstvwxyz";
         // Recorremos cada carácter del texto
@@ -68,28 +83,135 @@ public class ProcesadorDeTexto {
     }
 
     //Punto F - Validar cantidad de caracteres
-    private boolean validarLongitud(String textoIngresado) {
-        return textoIngresado.split(" ").length <= 140;
-
+    public boolean validarLongitud() {
+        //función que retorne true si la cantidad de palabras es menor o igual a 140
+        return this.texto.length() <= 140;
+        //return this.texto.split(" ").length <= 140;
     }
 
-
-
-    public static void guardarTexto(String texto) {
+    public void AgregarFrasesALaLista() {
         // Implementar
+        // Dividir el texto en frases usando el punto, coma o ? como delimitador.
+        String[] frases = texto.split("[.,!?]");
+
+        // Recorrer el array de frases y agregar cada una a la lista de Strings.
+        for (String frase : frases) {
+            // Trim elimina espacios en blanco adicionales
+            frase = frase.trim();   //agregar punto al final de la frase
+
+            if (!frase.isEmpty()) {
+                listaFrases.add(frase + ".");
+            }
+        }
+    }
+    //función que recorra la listaFrases y la enumere.
+    public void EnumerarFrases() {
+        // Implementar
+        // Recorrer la lista de frases y mostrar cada una con un número de índice
+        int indice = 1;
+        for (String frase : listaFrases) {
+            System.out.println("Frase " + indice + "): " + frase );
+            indice++;
+        }
     }
 
-    public static void imprimirTotalDeFrases() {
-        // Implementar
+
+
+    public void generarFichcero() {
+
+        try (FileWriter fileWriter = new FileWriter(this.fileName)) {
+            // Itera sobre cada objeto Conteo en la lista
+            //for que recorra la listaFrases y las guarde en el archivo
+
+            for (String frase : listaFrases) {
+                fileWriter.write(frase + "\n"); // Escribe el texto de la frase en el archivo
+
+            }
+            //fileWriter.write(procesador.getTexto());
+            fileWriter.close();
+            System.out.println("Texto guardado en " + this.fileName);
+            //mostrar contenido del archivo.
+
+
+
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error al guardar el archivo: " + e.getMessage());
+        }
+
+
     }
 
-    public static void seleccionarFrase() {
-        // Implementar
+    public void imprimirTotalDeFrases() {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String linea;
+            System.out.println("Contenido del archivo:");
+
+            int indice = 1;
+            while ((linea = reader.readLine()) != null) {
+                //mostrar número de frase
+                System.out.println("Frase " + indice + "): " + linea);
+                indice++;
+            }
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error al leer el archivo.");
+            e.printStackTrace();
+        }
     }
 
-    public static void generarFichero(String texto) {
+    public void seleccionarFrase() {
         // Implementar
+        // Punto i - Permitir al usuario seleccionar frases de un listado, editarlas y guardarlas
+        //String fileName = "./TP2/src/archivoTexto/Frases.txt";  // Nombre del archivo
+
+
+        // Leer el archivo y mostrar el contenido
+        //pedir numero de frase
+        System.out.println("Por favor, ingrese el número de la frase que desea editar:");
+        int numeroFrase = scanner.nextInt();
+        scanner.nextLine(); // Limpiar el buffer del scanner
+
+        BuscarFraseYModificar(numeroFrase);
+
+
     }
+
+    private void BuscarFraseYModificar(int numeroFrase) {
+        // Implementar
+
+        //recorrer el atributo this.listaFrases y mostrar la frase que corresponda al número ingresado}
+        for (int i = 0; i < listaFrases.size(); i++) {
+            if (i == numeroFrase - 1) {
+                System.out.println("Frase seleccionada: " + listaFrases.get(i));
+                System.out.println("Por favor, ingrese la nueva frase:");
+                String nuevaFrase = scanner.nextLine();
+                listaFrases.set(i, nuevaFrase);
+                System.out.println("Frase modificada con éxito.");
+                break;
+            }
+        }
+
+    }
+
+
+
+    //métodos get y set
+    public String getTexto() {
+        return this.texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    //get y set de fileName
+    public String getFileName() {
+        return this.fileName;
+    }
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
 
 
 }
